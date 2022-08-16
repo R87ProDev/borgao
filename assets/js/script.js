1a9d8f94e46e6e34681b2b78e4e698ca3683ae43
João Vitor Ribeiro <joaotvrodeio@gmail.com>
2022-08-16 09:35:43 -0300

    Teste teclado

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@ $(".btn-clock").click(function() {
     $(".btn-clock").addClass("btn-disabled");
     $(".btn-declassify").hide();
 });
+
+
+
+$('#animal').keypress(function(event) {
+    event.preventDefault();
+    return false;
+});
