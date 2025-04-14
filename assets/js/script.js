2490c11374ceed9239d62438acc978a0d1e60215
João Vitor Ribeiro <joaotvrodeio@gmail.com>
2025-04-14 09:04:15 -0300

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,24 @@
 /* Bloqueia o teclado mobile */
 $('.input-data').attr('readonly', 'readonly');
 let buckoff = 0;
+
+$("#fullScreen").click(function(){
+    document.body.requestFullscreen();
+})
+
+let myParam;
+
+const urlParams = new URLSearchParams(window.location.search);
+
+myParam = urlParams.get("j");
+
+
+$("#btn-save").attr("disabled",true)
+
 $(document).ready(function() {
+    $("#animal").val('')
+    $("#competitor").val('')
+    $(".ride-warning").hide()
     stateNote()
     $(".clear-animal").hide()
     $(".clear-competitor").hide()
@@ -10,13 +27,15 @@ $(document).ready(function() {
     var socket = null;
     let judgeID = 0;
     let rideID = 0;
+    let scores = 0;
     let time = 0;
+    let timeRide = 0
     let judgeClassification;
     const params = new URLSearchParams(window.location.search);
-    // connect();
+    connect();
 
     function connect() {
-        $('.data-note-template').hide();
+      /*   $('.data-note-template').hide(); */
         $('.circle').css('background-color', '#eab308');
         $('.status').text("Conectando...").css('color', '#eab308');
         if (params.has('j')) {
@@ -52,6 +71,9 @@ $(document).ready(function() {
             }
             /* BLoquear tudo = quando completa a montaria  */
             if (msg.data.includes("[completed]")) {
+                if(myParam == 5 || myParam == 6) {
+                    $('.data-note-template').hide();
+                }
                 disabledButtons()
                 $("#btn-save").attr("disabled", true);
                 $("#animal").attr("disabled", true);
@@ -65,7 +87,16 @@ $(document).ready(function() {
                 if (time === 0) {
                     $('.btn-clock').text("CLOCK");
                 }
+                if(time == 29) {
+                    $(".btn-clock").attr("disabled",true)
+                }
             }
+            /* Tempo de 8s */
+            if(msg.data.includes("[timing]")) {
+                timeRide = msg.data.replace('[timing]', '')
+                $('.time-ride').text(timeRide);
+            }
+               
             if (msg.data.includes('[judge-classification]')) {
                 judgeClassification = msg.data.replace('[judge-classification]', '');
 
@@ -91,20 +122,32 @@ $(document).ready(function() {
                 } else {
                     $('.data-note-template').hide();
                     $('.clock-template').show();
+               
+                }
+                if(myParam == 5 || myParam == 6) {
+                    $('.data-note-template').hide();
                 }
             }
             /* Competidor nao montou / Clock em tela */
             if (msg.data.includes("[ride]")) {
-
+                
+                $("#btn-save").attr("disabled",true)
+                $('.btn-clock').text("CLOCK");
                 if (judgeClassification == 'Arena') {
                     $('.data-note-template').show();
                     $('.clock-template').hide();
+                    
                 } else {
                     $(".btn-clock").attr("disabled", false);
                     $('.data-note-template').hide();
                     $('.clock-template').show();
+                    $(".btn-clock").show()
+                    /* $(".btn-declassify").show() */
+                    
                 }
-
+                if(myParam == 5 || myParam == 6) {
+                    $('.data-note-template').hide();
+                }
                 /* $('#score-unique').removeAttr('disabled');
                 $('#btnSend').removeAttr('disabled');
                 $('#scorePanel').hide();
@@ -112,38 +155,71 @@ $(document).ready(function() {
             }
             /* Iniciou o cronometro e bloqueia botao de clock */
             if (msg.data.includes("[started]")) {
-                $('.btn-clock').hide();
+                buckoff = 0
+                $('.clock-template').hide();
+                $(".ride-warning").show()
+                if(myParam == 5 || myParam == 6) {
+                    $('.data-note-template').hide();
+                }
+                
             }
             /* Quando o peao cair/ Desabilita nota competidor */
             if (msg.data.includes("[buckoff]")) {
+                $("#competitor").val("00,00")   
                 $("#competitor").attr("disabled", true);
+                $("#number-1").attr("disabled", false);
+                $("#number-2").attr("disabled", false);
                 $("#number-0").attr("disabled", false);
+                $("#number-0").attr("disabled", false);
+                $(".ride-warning").hide()
+                
                 buckoff = 1
+                if(judgeClassification == "Brete") {
+                    /* $(".clock-template").show()
+                    $(".btn-clock").hide() */
+
+                }
+                if(myParam == 5 || myParam == 6) {
+                    $('.data-note-template').hide();
+                }
+
             }
             /*  Quando a montaria acaba / Habilita o quadro de nota  */
             if (msg.data.includes("[finished]")) {
-                disabledButtons()
-                $("#btn-save").attr("disabled", false);
+                $("#competitor").val("")
+                $('.btn-clock').text("CLOCK");
+                $(".ride-warning").hide()
+               
+                if(buckoff == 1) {
+                    disabledButtons()
+                    
+                } else {
+
+                    $("#number-1").attr("disabled", false);
+                    $("#number-2").attr("disabled", false);
+                    $("#number-3").attr("disabled", true);
+                    $("#number-3").attr("disabled", true);
+                    $("#number-4").attr("disabled", true);
+                    $("#number-5").attr("disabled", true);
+                    $("#number-6").attr("disabled", true);
+                    $("#number-7").attr("disabled", true);
+                    $("#number-8").attr("disabled", true);
+                    $("#number-9").attr("disabled", true);
+                    $("#number-0").attr("disabled", false);
+                    $("#number-25").attr("disabled", true);
+                    $("#number-50").attr("disabled", true);
+                    $("#number-75").attr("disabled", true);
+                }
+               
 
                 $("#animal").attr("disabled", false);
                 $("#competitor").attr("disabled", false);
                 $('.data-note-template').show();
                 $('.clock-template').hide();
-            }
-
-            /* if (msg.data.includes("[score-numbers]")) {
-                let rules = msg.data.replace("[score-numbers]", "");
-                rules = msg.data.replace(/\D/g, "");
-                judges = rules.substring(0, 1);
-                scores = rules.substring(1, 2);
-                if (scores == 1 && $('#scorePanel').css('display') != "none") {
-                    $('#1score').show();
-                    $('#2scores').hide();
-                } else if (scores == 2 && $('#scorePanel').css('display') != "none") {
-                    $('#1score').hide();
-                    $('#2scores').show();
+                if(myParam == 5 || myParam == 6) {
+                    $('.data-note-template').hide();
                 }
-            } */
+            }
 
         }
         socket.onclose = function() {
@@ -165,6 +241,31 @@ $(document).ready(function() {
             //$('#error-alert').show();
         }
     }
+
+    $(".btn-confirm-declassify").click(function() {
+        let declassifySelect;
+        $("#declassifySelect option:selected").each(function(){
+            /* Motivo de desclassificacao */
+            declassifySelect = $(this).val()
+        })
+        var sendData = {
+            type: "penalty",
+            numbers: scores,
+            judgeId: judgeID,
+            rideId: rideID,
+            date: Date.now(),
+            reason: declassifySelect,
+        };
+        setTimeout(function() {
+            socket.send(JSON.stringify(sendData));
+        }, 100);
+        console.log("sendata",sendData)
+        $(".clock-template").hide()
+        $("#animal").attr("disabled",true)
+        $(".data-note-template").show()
+        disabledButtons()
+        $("#btn-save").attr("disabled",true)
+    })
     /* Clock */
     $(".btn-clock").click(function() {
         $(".btn-clock").attr("disabled", true);
@@ -181,40 +282,32 @@ $(document).ready(function() {
         setTimeout(function() {
             socket.send(JSON.stringify(sendData));
         }, 100);
+        console.log("sendata",sendData)
     });
 
     $("#btn-save").click(function() {
         /* Duas notas cada juiz */
-        if (scores == 2) {
-            var sendData = {
-                type: "score",
-                numbers: scores,
-                judgeId: judgeID,
-                rideId: rideID,
-                date: Date.now(),
-                scoreAnimal: $('#animal').val(),
-                scoreRider: $('#competitor').val(),
-            };
-            /* Uma nota cada juiz */
-        } else {
-            var sendData = {
-                type: "score",
-                numbers: scores,
-                judgeId: judgeID,
-                rideId: rideID,
-                date: Date.now(),
-                score: $('#score-unique').val(),
-            };
-        }
-
+        var sendData = {
+            type: "score",
+            numbers: scores,
+            judgeId: judgeID,
+            rideId: rideID,
+            date: Date.now(),
+            scoreAnimal: $('#animal').val(),
+            scoreRider: $('#competitor').val(),
+        };
+         $('.btn-clock').text("CLOCK");
+        localStorage.setItem("typeNote","animal")
         setTimeout(function() {
             socket.send(JSON.stringify(sendData));
         }, 100);
+        console.log("sendata",sendData)
 
-        $('#score1').val(null).attr('disabled', 'disabled');
-        $('#score2').val(null).attr('disabled', 'disabled');
+        $('#animal').val(null).attr('disabled', true);
+        $('#competitor').val(null).attr('disabled', true);
         $('#score-unique').val(null).attr('disabled', 'disabled');
-        $('#btnSend').attr('disabled', 'disabled');
+        $('#btn-save').attr('disabled', true);
+
     })
 })
 
@@ -236,8 +329,7 @@ const stateNote = (stateType = localStorage.getItem("typeNote")) => {
 
 const dataNumber = (number, noteType = localStorage.getItem("typeNote")) => {
     let numberLength = $(`#${noteType}`).val().length;
-    console.log(number,"ok")
-
+   
     if (numberLength === 0) {
         $(`#${noteType}`).val(number)
         $(`.clear-${localStorage.getItem("typeNote")}`).show()
@@ -278,48 +370,76 @@ const dataNumber = (number, noteType = localStorage.getItem("typeNote")) => {
 
     } else if (numberLength === 1) {
         $(`#${noteType}`).val($(`#${noteType}`).val() + number + ',')
-        console.log("digito segundo numero")
-        $("#number-1").attr("disabled", true);
-        $("#number-2").attr("disabled", true);
-        $("#number-3").attr("disabled", true);
-        $("#number-4").attr("disabled", true);
-        $("#number-5").attr("disabled", true);
-        $("#number-6").attr("disabled", true);
-        $("#number-7").attr("disabled", true);
-        $("#number-8").attr("disabled", true);
-        $("#number-9").attr("disabled", true);
-
-
-    } else if (numberLength === 3) {
+       // console.log($(`#${noteType}`).val())
+        if($(`#${noteType}`).val().replace(',','') == 25) {
+            $("#number-1").attr("disabled", true);
+            $("#number-2").attr("disabled", true);
+            $("#number-3").attr("disabled", true);
+            $("#number-4").attr("disabled", true);
+            $("#number-5").attr("disabled", true);
+            $("#number-6").attr("disabled", true);
+            $("#number-7").attr("disabled", true);
+            $("#number-8").attr("disabled", true);
+            $("#number-9").attr("disabled", true);
+            $("#number-0").attr("disabled", false);
+            $("#number-25").attr("disabled", true);
+            $("#number-50").attr("disabled", true);
+            $("#number-75").attr("disabled", true);
+        } else {
+            $("#number-1").attr("disabled", true);
+            $("#number-2").attr("disabled", true);
+            $("#number-3").attr("disabled", true);
+            $("#number-4").attr("disabled", true);
+            $("#number-5").attr("disabled", true);
+            $("#number-6").attr("disabled", true);
+            $("#number-7").attr("disabled", true);
+            $("#number-8").attr("disabled", true);
+            $("#number-9").attr("disabled", true);
+            $("#number-0").attr("disabled", false);
+            $("#number-25").attr("disabled", false);
+            $("#number-50").attr("disabled", false);
+            $("#number-75").attr("disabled", false);
+        }
+    } else if (numberLength >= 3) {
         if (number == 0) {
-            $(`#${noteType}`).val($(`#${noteType}`).val() + number)
+            $(`#${noteType}`).val($(`#${noteType}`).val() + number + 0)
             $(`.clear-${localStorage.getItem("typeNote")}`).hide()
             $("#competitor").focus()
             localStorage.setItem("typeNote", "competitor")
-            console.log($(`#${noteType}`).val(), "number")
-            if ($(`#${noteType}`).val() >= 25) {
+            /* buckoff == 1 == Cair */
+            if (buckoff != 1) {
+                $("#competitor").attr("disabled", false);
+                $("#competitor").focus()
+                $("#number-1").attr("disabled", false);
+                $("#number-2").attr("disabled", false);
+                $("#number-0").attr("disabled", false);
                 $("#number-25").attr("disabled", true);
                 $("#number-50").attr("disabled", true);
                 $("#number-75").attr("disabled", true);
+                $("#btn-save").attr("disabled",false)
+                console.log("entrou aqui")
+                if(noteType === "competitor") {
+                    $("#btn-save").attr("disabled",false)
+                }
             } else {
-                $("#number-25").attr("disabled", false);
-                $("#number-50").attr("disabled", false);
-                $("#number-75").attr("disabled", false);
-            }
-            /* buckoff == 1 == Cair */
-            if (buckoff != 1) {
-                console.log(buckoff)
-                $("#competitor").attr("disabled", false);
-                $("#competitor").focus()
-
-            } else {
+                console.log("entrou aqui 2")
                 $("#competitor").attr("disabled", true);
                 disabledButtons()
+                $("#btn-save").attr("disabled",false)
+                
+            }
+            if (noteType === "competitor" && $(`#${noteType}`).val().length >=2) {
+                $(`.clear-${localStorage.getItem("typeNote")}`).hide()
+                disabledButtons()
+                $("#btn-save").attr("disabled",false)
+                $("#competitor").blur()
+                
+
             }
 
             stateNote()
         }
-        if (number >= 25) {
+        else if (number >= 25) {
             $(`#${noteType}`).val($(`#${noteType}`).val() + number)
             $(`.clear-${localStorage.getItem("typeNote")}`).hide()
 
@@ -329,15 +449,28 @@ const dataNumber = (number, noteType = localStorage.getItem("typeNote")) => {
                 console.log(buckoff)
                 $("#competitor").attr("disabled", false);
                 $("#competitor").focus()
+                $("#number-1").attr("disabled", false);
+                $("#number-2").attr("disabled", false);
+                $("#number-0").attr("disabled", false);
+                $("#number-25").attr("disabled", true);
+                $("#number-50").attr("disabled", true);
+                $("#number-75").attr("disabled", true);
+                if(noteType === "competitor") {
+                    $("#btn-save").attr("disabled",false)
+                }
             } else {
                 $("#competitor").attr("disabled", true);
                 disabledButtons()
+                $("#btn-save").attr("disabled",false)
             }
 
-            /*    $(".data-number").addClass("d-none") */
             stateNote()
-        } else {
+        } else { 
             $(`#${noteType}`).val($(`#${noteType}`).val() + number)
+        }
+        if (noteType === "competitor" && $(`#${noteType}`).val().length >=2) {
+            $(`.clear-${localStorage.getItem("typeNote")}`).hide()
+            disabledButtons()
         }
     }
     /* else if (numberLength === 4) {
@@ -355,11 +488,14 @@ const dataNumber = (number, noteType = localStorage.getItem("typeNote")) => {
        } */
 }
 // Limpa o campo de notas
-const cleanInput = (typeInput) => {
+const cleanInput = (typeInput = localStorage.getItem("typeNote")) => {
+    console.log(typeInput,"olaaaa")
+    $("#btn-save").attr("disabled",true)
     let numberLength = $(`#${typeInput}`).val().length;
     $(`#${typeInput}`).val('')
+
     if (numberLength >= 1) {
-        $(`.clear-${localStorage.getItem("typeNote")}`).hide()
+      /*   $(`.clear-${localStorage.getItem("typeNote")}`).hide() */
         $("#number-1").attr("disabled", false);
         $("#number-2").attr("disabled", false);
         $("#number-3").attr("disabled", true);
@@ -370,23 +506,36 @@ const cleanInput = (typeInput) => {
         $("#number-8").attr("disabled", true);
         $("#number-9").attr("disabled", true);
         $("#number-0").attr("disabled", false);
-
+        $("#number-25").attr("disabled", true);
+        $("#number-50").attr("disabled", true);
+        $("#number-75").attr("disabled", true);
+        console.log("Entro ")
         stateNote()
     }
 }
 
-$("#animal, #competitor").focus(function() {
+$("#animal, #competitor").on("focus click",function() {
+    console.log("foca porra")
+    console.log($(this)[0].id)
+
+   
     let cleanInput = $(this).val().length
+    console.log(cleanInput,"clean input")
     localStorage.setItem("typeNote", $(this)[0].id)
-    if (cleanInput >= 1) {
+    if (cleanInput >= 2) {
+        console.log("nao nao")
         $(`.clear-${$(this)[0].id}`).show()
     }
+    if($("#competitor").val().length >= 4 && localStorage.getItem("typeNote") == "competitor") {
+        $("#btn-save").attr("disabled",false)
+    }
+    
 })
 
-$("#animal, #competitor").focusout(function() {
+/* $("#animal, #competitor").focusout(function() {
     $(`.clear-competitor`).hide()
 })
-
+ */
 const disabledButtons = () => {
     $("#number-1").attr("disabled", true);
     $("#number-2").attr("disabled", true);
@@ -402,3 +551,10 @@ const disabledButtons = () => {
     $("#number-50").attr("disabled", true);
     $("#number-75").attr("disabled", true);
 }
+
+/* $(".btn-declassify ").click(function () {
+    $(".btn-confirm-declassify").attr("data-bs-dismiss",'modal')
+    $(".btn-confirm-declassify").attr("aria-label",'Close')
+})
+ */
+/* data-bs-dismiss="modal" aria-label="Close" */
